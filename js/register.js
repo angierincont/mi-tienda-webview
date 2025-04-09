1f709a95d0c1299b07704d793754bece21d8653c
angierincont <angie.rincont@uniagustiniana.edu.co>
2025-04-09 21:14:57 +0000

    2

--- js/register.js
+++ js/register.js
@@ -1,6 +1,20 @@
-function registrarUsuario(e) {
-    e.preventDefault();
-    alert('¡Registro exitoso!');
-    e.target.reset();
-  }
-  
+const formRegistro = document.getElementById("form-registro");
+
+formRegistro.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const datos = {
+    nombre: formRegistro.elements[0].value,
+    apellido: formRegistro.elements[1].value,
+    correo: formRegistro.elements[2].value,
+    usuario: formRegistro.elements[3].value,
+    contraseña: formRegistro.elements[4].value,
+    ciudad: formRegistro.elements[5].value,
+    telefono: formRegistro.elements[6].value,
+  };
+
+  console.log("Usuario registrado:", datos);
+  alert("¡Registro exitoso!");
+
+  formRegistro.reset();
+});
