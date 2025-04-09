835cae158dffe944a1b383b8d47ee410ce7ac11e
angierincont <angie.rincont@uniagustiniana.edu.co>
2025-04-09 21:14:32 +0000

    7

--- js/main.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', async () => {
-  const productos = await obtenerProductos();
-  cargarProductos(productos);
-  document.getElementById('buscador').addEventListener('input', buscarProductos);
-  document.getElementById('filtroCategoria').addEventListener('change', () => cargarProductos(productos));
-});
