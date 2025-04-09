c6a0deef56a0ebcf9c8d42c61cdc05d767d006d8
angierincont <angie.rincont@uniagustiniana.edu.co>
2025-04-09 21:28:02 +0000

    5

--- js/Feature.js
+++ js/Feature.js
@@ -1,12 +1,11 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const toggleModoBtn = document.getElementById("toggle-modo");
-  toggleModoBtn.addEventListener("click", () => {
-    document.body.classList.toggle("dark");
+const toggleModoBtn = document.getElementById("toggle-modo");
 
-    if (document.body.classList.contains("dark")) {
-      toggleModoBtn.textContent = "☀️ Modo claro";
-    } else {
-      toggleModoBtn.textContent = "🌙 Modo oscuro";
-    }
-  });
+toggleModoBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    toggleModoBtn.textContent = "☀️ Modo claro";
+  } else {
+    toggleModoBtn.textContent = "🌙 Modo oscuro";
+  }
 });
