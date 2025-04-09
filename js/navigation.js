dcf37866e6b918ae85db48ae0b6441566c6e5d10
angierincont <angie.rincont@uniagustiniana.edu.co>
2025-04-09 21:14:41 +0000

    9

--- js/navigation.js
+++
@@ -1,8 +0,0 @@
-function mostrarPestaña(indice) {
-    const pestañas = document.querySelectorAll('.pestaña');
-    pestañas.forEach(p => p.classList.remove('activa'));
-    pestañas[indice].classList.add('activa');
-  
-    if (indice === 1) mostrarFavoritos(); // si abre "Favoritos", actualiza la vista
-  }
-  
